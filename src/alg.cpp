a6fc0f1d69f04a0f1288c20be8fab19b1c392bf5
BardovNikita <116936669+BardovNikita@users.noreply.github.com>
2023-03-12 00:08:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,17 +6,17 @@
 double pown(double value, uint16_t n) {
   if (n == 0) {
     return 1;
-	} else {
+  } else {
     return value * pown(value, n-1);
-	}
+  }
 }
 
 uint64_t fact(uint16_t n) {
   if ((n == 1) || (n == 0)) {
     return 1;
-	} else {
+  } else {
     return n * fact(n - 1);
-	}
+  }
 }
 
 double calcItem(double x, uint16_t n) {
@@ -26,8 +26,8 @@ double calcItem(double x, uint16_t n) {
 double expn(double x, uint16_t count) {
   double sum = 1.0;
   for (count; count > 0; count--) {
-    sum += calcItem(x,count);
-	}
+    sum += calcItem(x, count);
+  }
     return sum;
 }
 
@@ -35,7 +35,7 @@ double sinn(double x, uint16_t count) {
   double sum = 0;
   for (count; count > 0; count--) {
     sum += pown(-1, count-1) * calcItem(x, 2*count-1);
-	}
+  }
     return sum;
 }
 
